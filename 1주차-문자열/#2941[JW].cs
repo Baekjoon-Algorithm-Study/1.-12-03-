c449860669571f7cb43099ab71dc3c5176f41b0d
KimJinWooDa <76438011+KimJinWooDa@users.noreply.github.com>
2021-11-30 16:48:02 +0900

    [김진우]

---
+++ 1주차-문자열/#2941[JW].cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class StringReverse
+{
+    static void Main()
+    {
+        string[] croatia = new string[8] { "c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z=" };
+        string str = Console.ReadLine();
+        
+        for (int i = 0; i < croatia.Length; i++)
+        {
+            if (str.Contains(croatia[i])) str = str.Replace(croatia[i], "#");
+        }
+
+        int count = str.Length;
+        Console.WriteLine(count);
+    }
+}
