4605c23d112e05bbc8917cc84f2a01786fb5909b
KimJinWooDa <76438011+KimJinWooDa@users.noreply.github.com>
2021-12-25 23:22:12 +0900

    Create #1712(jw).cs

---
+++ 2주차-기초 수학1/#1712(jw).cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaekJoon.Baekjoon._2주차_기초수학1
+{
+    class Class1
+    {
+        static int count=0;
+        static void Main()
+        {
+            string[] order = Console.ReadLine().Split();
+
+            int A = int.Parse(order[0]);
+            int B = int.Parse(order[1]);
+            int C = int.Parse(order[2]);
+           
+            if (B >= C) {
+                count = -1;
+                Console.WriteLine(count);
+                return;
+            }
+            else
+            {
+                count = A/(C-B)+1;
+                Console.Write(count);
+            }
+        }
+    }
+}
