cc7c8e2b9e142b8b9f028f867f8dd487152cfe8b
KimJinWooDa <76438011+KimJinWooDa@users.noreply.github.com>
2021-12-30 17:01:43 +0900

    [김진우]

--- 2주차-기초 수학1/10757.cs
+++ 2주차-기초 수학1/10757.cs
@@ -5,9 +5,13 @@ namespace BaekJoon.Baekjoon._2주차_기초_수학1
 {
     class _10757
     {
-        static void Main()
+        static void Main(string[] args)
         {
-
+            string[] str = Console.ReadLine().Split();
+            
+            BigInteger idx1 = BigInteger.Parse(str[0]);
+            BigInteger idx2 = BigInteger.Parse(str[1]);
+            Console.WriteLine(idx1 + idx2);
         }
     }
 }
