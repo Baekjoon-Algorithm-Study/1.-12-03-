c32c510d12e9c4ce4e6a0ceadfa2f9d2ecce0b78
KimJinWooDa <76438011+KimJinWooDa@users.noreply.github.com>
2022-01-07 00:14:49 +0900

    [김진우]

---
+++ 3주차-기초 수학2/#1978(jw).cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class _1978
+    {
+        static void Main(string[] args) {
+            int counting = int.Parse(Console.ReadLine());
+
+            string[] numbers = Console.ReadLine().Split();
+            
+            int decimalCount=0;
+
+            for (int i = 0; i < counting; i++)
+            {
+                int A = int.Parse(numbers[i]);
+                bool B = true;
+                if (A == 1) continue;
+                if (A==2 || A==3)
+                {
+                    decimalCount++;
+                    continue;
+                }
+                int two = 2;
+                while (two != A-1)
+                {
+                    B = A % two++ == 0 ? false : true;
+                    if (!B) break;
+                }
+                if (B) decimalCount++;
+            }
+            Console.WriteLine(decimalCount);
+        }
+
+    }
+}
