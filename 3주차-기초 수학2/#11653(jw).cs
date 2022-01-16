f76b93f5279714ae9ab7cdc6defae081b721bea0
KimJinWooDa <76438011+KimJinWooDa@users.noreply.github.com>
2022-01-16 18:26:40 +0900

    [김진우]

---
+++ 3주차-기초 수학2/#11653(jw).cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class _1978
+    {
+        static void Main(string[] args)
+        {
+            int M = int.Parse(Console.ReadLine());
+
+            for (int i = 2; i <= M; i++)
+            {
+                if (M % i == 0)
+                {
+                    Console.WriteLine(i);
+                    M /= i;
+                    i--;
+                }
+            }
+            if (M > 1) Console.WriteLine(M);
+
+        }
+    }
+}
